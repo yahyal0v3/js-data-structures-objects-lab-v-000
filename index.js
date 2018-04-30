acadd18390fae5928bb78214a047fe34a8abe376
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-30 17:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ driver = {}
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, {[key]: value})
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, {[key]: value})
+}
